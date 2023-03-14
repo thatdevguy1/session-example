444dec77530e780e86cefb7f6c85f7f2806467bc
David Bland <dave@thatdevguy.me>
2023-03-14 14:27:36 -0400

    express sessions with mongo store

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const users = [
+  { username: "bob", password: "111" },
+  { username: "jim", password: "111" },
+];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/login", function (req, res, next) {
+  const foundUser = users.find((user) => user.username === req.body.username);
+  // const foundUser2 = users.find(function(user){
+  //   return user.username === req.body.username
+  // });
+
+  if (foundUser && foundUser.password === req.body.password) {
+    // delete foundUser.password
+    req.session.user = foundUser;
+  }
+
+  res.redirect("/");
+});
+
+router.get("/logout", function (req, res) {
+  req.session.destroy();
+  res.redirect("/");
 });
 
 module.exports = router;
